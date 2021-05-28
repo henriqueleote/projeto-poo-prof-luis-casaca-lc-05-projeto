2b2f960f1fe5c2ea6981666a87fe0fff2fc4475f
GoncaloFernandes200221047 <gonsisfernandes8@gmail.com>
2021-05-28 17:32:04 +0100

    Commit #9
    Atribuição de pontos ao jogador

--- boats/src/boats/Pontuacao.java
+++ boats/src/boats/Pontuacao.java
@@ -38,4 +38,25 @@ public class Pontuacao {
     public String toString(){
         return "\nPontos: " + getPoints() + "\nRecord: " + getHigh();
     }
+   
+    public void gameEndVerification(int points){
+        // Verificação do preenchimento do tabuleiro
+        /* Caso os espaços não estejam todos corretamente preenchidos, o jogo continua.
+        -> Criação da variável "int numberOfFails" que deduzirá 10 pontos ao jogador por cada tentativa falhada.
+        -> Criação da variável "int pointsToDeduct", que deduzirá à pontuação final do jogador:
+        -> 2 pontos por falha na validação de casas do tipo "Água"; 
+        -> 5 pontos por cada segmento de um barco falhado;
+        */
+        
+        /* Caso os espaços do tabuleiro estejam todos preenchidos corretamente, o jogo termina e a pontuação final é calculada:
+        -> Criação da variável "int startingPontuation", que atribui uma pontuação base consoante o nível de dificuldade do jogo; */
+    
+        /* -> Criação da variável "int bonusPontuation", onde serão adicionados:
+        -> 20 pontos caso jogador tenha validado o tabuleiro corretamente à primeira tentativa (numberOfFails = 0);
+        -> Por cada segundo ultrapassado em relação ao tempo base, deduzir 1 ponto à pontuação final; por cada segundo que o jogador não tenha utilizado, incrementar 1 ponto à pontuação final */
+        
+        /* -> Criação da variável "int finalPontuation", que calculará a pontuação final do jogador através da seguinte equação: finalPontuation = startingPontuation - pointsToDeduct + bonusPontuation
+        -> Se o valor da variável finalPontuation for um número negativo, colocar a variável finalPontuation com o valor 0 (int finalPontuation = 0) */
+        
+    }
 }
